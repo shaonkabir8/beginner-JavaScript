f04f916b9963675c8ff7a33f541cea8d6876de66
Shaonkabir <shaonkabir98@gmail.com>
2020-04-12 13:30:44 +0600

    refactor: 🔥explain scope with example

--- playground/scope.js
+++ playground/scope.js
@@ -11,40 +11,44 @@
 // go();
 /* eslint-disable*/
 
-// function isCool(name) {
-//   if (name === 'wes') {
-//     var cool = true;
-//   }
-//   console.log(cool);
-//   return cool;
-// }
-
-// for(let i = 0; i < 10; i++) {
-//   console.log(i);
-// }
-
-
-const dog = 'snickers';
-
-function logDog(dog) {
-  console.log(dog);
+for(let i = 0; i < 10; i++) {
+  console.log(`index is: ${i}`)
 }
 
-function go() {
-  const dog = 'sunny';
-  logDog('sunny');
+// isCool function for better understanding of `JavaScript Scopping`
+const isCool = name => {
+  
+  // Check person is cool or not!
+  console.log(`✈ CHECKING..... ${name} is Cool or Not!`)
+  
+  if(name === 'Shaon') {
+    // ! if we use `let` variable instead of `var`, It'll show a RefferenceError...
+    // ! Because `let` is `Block` Scoped. 
+    // ! to solve this problem, we'll have to define `let` variable in upper label [outside of `if` Block]
+    // ! Easies option is to use `var`
+    // ! Because it is `function scoped` not `Block scoped`.
+    var cool = 'Cool 🔥';
+    var message = "✔ Yesss! "
+  } else {
+    // ! if NOT COOL, update informations
+    cool = "Not Cool 😶"
+    message = "No"
+  }
+  
+  // global statement for both Logic.. (if cool or not!) 
+  console.log(`${message}! ${name} is ${cool}`);
+  return cool;
 }
 
-go();
-
-function yell() {
-  console.log(name.toUpperCase())
-}
-
-function sayHi(name) {
-
-  yell();
-}
+isCool('Shaon')
 
 
-yell();
+
+
+
+
+
+
+
+
+
