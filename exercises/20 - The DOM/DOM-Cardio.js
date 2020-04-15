b0fe9db436fb75cf5a884d09d530115b7f5e8dbb
Shaonkabir <shaonkabir98@gmail.com>
2020-04-15 21:04:42 +0600

    refactor: 🔥DOM CARDIO practice

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,25 +1,49 @@
+/* eslint-disable*/
+
 // Make a div
-
+const div = document.createElement('div');
 // add a class of wrapper to it
-
+div.classList.add('wrapper')
 // put it into the body
+const body = document.querySelector('body')
+body.appendChild(div);
 
 // make an unordered list
-
+const list = document.createElement('ul')
 // add three list items with the words "one, two three" in them
+const listItemOne = document.createElement('li')
+listItemOne.textContent = "one"
+const listItemTwo = document.createElement('li')
+listItemTwo.textContent = "two"
+const listItemThree = document.createElement('li')
+listItemThree.textContent = "three"
+list.append(listItemOne,listItemTwo,listItemThree)
 // put that list into the above wrapper
+div.appendChild(list)
 
 // create an image
-
+const image = document.createElement('img')
 // set the source to an image
+image.setAttribute('src', 'https://placehold.it/300')
 // set the width to 250
+image.style.width = "250px";
 // add a class of cute
+image.classList.add('cute')
 // add an alt of Cute Puppy
+image.setAttribute('alt', "Cute Puppy")
 // Append that image to the wrapper
+div.appendChild(image)
 
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
+const paragraphDiv = `
+    <div>
+        <p class="warning">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ab.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ab.</p>
+    </div>
+`
 
+list.insertAdjacentHTML('beforebegin', paragraphDiv)
 // add a class to the second paragraph called warning
 // remove the first paragraph
 
