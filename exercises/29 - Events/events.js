fab1a0ac44617b6a1ef3495674adba5fdbe6ab8c
Shaonkabir <shaonkabir98@gmail.com>
2020-04-16 22:12:52 +0600

    refactor: 🔥 prevent Bubbling when Events Fired!

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -24,6 +24,39 @@ const buyItem = () => {
   console.log("BUY ITEM 🔥 ")
 }
 
-buyButtons.forEach(btn => {
-  btn.addEventListener('click', buyItem)
+// buyButtons.forEach(btn => {
+//   btn.addEventListener('click', buyItem)
+// })
+
+
+
+// 
+
+
+
+
+// USE THIS TRICKS WHILE LOOPING THROUGH DATA
+const handleBuyItem = btn => {
+  btn.addEventListener('click', e => {
+    console.log("You're going to buy 🔥")
+
+    //grab data-attr from HTML markup
+    // convert "String" price to "Number" using parseFloat
+    const price = parseFloat(e.target.dataset.price);
+    console.log(`Your desire product price is: $${price}`)
+    
+
+    // 🔥 prevent Bubbling 
+    e.stopPropagation();
+
+  })
+}
+
+buyButtons.forEach(handleBuyItem)
+
+
+// Event for Window Click 🔥
+window.addEventListener('click', e => {
+  console.log(`YOU CLICKED ON: ${e.target.localName} 🔥`)
 })
+
