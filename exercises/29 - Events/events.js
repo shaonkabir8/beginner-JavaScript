caa92348c80f089cb9b2c11311fd27fc722f2bff
Shaonkabir <shaonkabir98@gmail.com>
2020-04-16 21:11:55 +0600

    refactor: 🔥Event Listerner

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -1,53 +1,29 @@
+/* eslint-disable */
+
 const butts = document.querySelector('.butts');
 const coolButton = document.querySelector('.cool');
 
+
 function handleClick() {
-  console.log('🐛 IT GOT CLICKED!!!');
+  console.log("BUTTON IS CLICKED ! 🔥")
 }
 
-const hooray = () => console.log('HOORAY!');
+// Arrow function for remove listerner test
+const hurray = () => console.log("🔥 HURRAY!!")
 
-butts.addEventListener('click', function() {
-  console.log('Im an anon!');
-});
-coolButton.addEventListener('click', hooray);
 
-butts.removeEventListener('click', handleClick);
+butts.addEventListener('click', handleClick);
 
-// Listen on multiple items
-const buyButtons = document.querySelectorAll('button.buy');
+coolButton.addEventListener('click', hurray)
 
-function handleBuyButtonClick(event) {
-  console.log('You clicked a button!');
-  const button = event.target;
-  // console.log(button.textContent);
-  // console.log(parseFloat(event.target.dataset.price));
-  console.log(event.target);
-  console.log(event.currentTarget);
-  console.log(event.target === event.currentTarget);
-  // Stop this event from bubbling up
-  // event.stopPropagation();
+
+// Dealing with MULTIPLE ITEMS
+const buyButtons = document.querySelectorAll('.buy');
+
+const buyItem = () => {
+  console.log("BUY ITEM 🔥 ")
 }
 
-buyButtons.forEach(function(buyButton) {
-  buyButton.addEventListener('click', handleBuyButtonClick);
-});
-
-window.addEventListener(
-  'click',
-  function(event) {
-    console.log('YOU CLICKED THE WINDOW');
-    console.log(event.target);
-    console.log(event.type);
-    // event.stopPropagation();
-    console.log(event.bubbles);
-  },
-  { capture: true }
-);
-
-const photoEl = document.querySelector('.photo');
-
-photoEl.addEventListener('mouseenter', e => {
-  console.log(e.currentTarget);
-  console.log(this);
-});
+buyButtons.forEach(btn => {
+  btn.addEventListener('click', buyItem)
+})
